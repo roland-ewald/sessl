ee6e29be758b0e39375d0ea827551fce4045a161
__roland__ <devnull@localhost>
2012-03-09 18:23:15 +0100

    refs #5: started working on result interpolation.

--- src/main/scala/sessl/sbmlsim/Instrumentation.scala
+++ src/main/scala/sessl/sbmlsim/Instrumentation.scala
@@ -1,8 +1,8 @@
 package sessl.sbmlsim
 
 import org.simulator.math.odes.MultiTable
-
 import sessl.util.SimpleInstrumentation
+import scala.collection.mutable.ListBuffer
 
 /** Support for 'instrumentation' of SBMLsimulator runs.
  *  It seems the simulators always provide the complete state vector for every computed step,
@@ -14,7 +14,43 @@ trait Instrumentation extends SimpleInstrumentation with ResultHandling {
 
   abstract override def considerResults(runId: Int, assignmentId: Int, results: MultiTable) {
     super.considerResults(runId, assignmentId, results)
-    //call def addValueFor[T](runID: Int, internalName: String, value: TimeStampedData)
+
+    //    println(getInterpolationIndices(results))
+
+    for (varName <- varsToBeObserved) {
+      val index = 0
+      //      while (iterator.nonEmpty) {
+      //        val obstime = iterator.next()
+      //
+      //        for (obsTime <- observationTimes) {
+      //          addValueFor(runId, varName, (obsTime, 1.0))
+      //        }
+      //      }
+    }
   }
 
+  //TODO: Move to utility class and test this!
+
+  def getInterpolationIndices(results: MultiTable): List[(Int, Int)] = {
+
+    if (observationTimes.isEmpty) return List()
+    val timePoints = results.getTimePoints()
+    if (timePoints.isEmpty) return List()
+
+    val timePointIndices = ListBuffer[(Int, Int)]()
+    var j = 0
+    for (i <- 1 until timePoints.length) {
+      while (j < observationTimes.length && timePoints(i) > observationTimes(j)) {
+        timePointIndices += ((i - 1, i))
+        j += 1
+      }
+    }
+
+    timePointIndices.toList
+  }
+
+}
+
+object Instrumentation {
+  val MIN_INSTRUMENTATION_PRECISION = 10e-02
 }
