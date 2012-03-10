62467ede5cb1f8fe25ac97445f6badf10908d5cc
__roland__ <devnull@localhost>
2012-03-10 11:33:48 +0100

    refs #5: finished instrumentation trait (untested).

--- src/main/scala/sessl/sbmlsim/Instrumentation.scala
+++ src/main/scala/sessl/sbmlsim/Instrumentation.scala
@@ -3,6 +3,7 @@ package sessl.sbmlsim
 import org.simulator.math.odes.MultiTable
 import sessl.util.SimpleInstrumentation
 import scala.collection.mutable.ListBuffer
+import sessl.util.Interpolation._
 
 /** Support for 'instrumentation' of SBMLsimulator runs.
  *  It seems the simulators always provide the complete state vector for every computed step,
@@ -14,24 +15,38 @@ trait Instrumentation extends SimpleInstrumentation with ResultHandling {
 
   abstract override def considerResults(runId: Int, assignmentId: Int, results: MultiTable) {
     super.considerResults(runId, assignmentId, results)
-
-    //    println(getInterpolationIndices(results))
-
-    for (varName <- varsToBeObserved) {
-      val index = 0
-      //      while (iterator.nonEmpty) {
-      //        val obstime = iterator.next()
-      //
-      //        for (obsTime <- observationTimes) {
-      //          addValueFor(runId, varName, (obsTime, 1.0))
-      //        }
-      //      }
+    for (
+      p <- findInterpolationPoints(results.getTimePoints(), observationTimes); v <- varsToBeObserved
+    ) {
+      addValueForPoint(runId, p, v, results)
     }
   }
 
+  /** Adds the observed value for a given interpolation point.
+   *  The overall width of the time interval is (t_2 - t_1), and the observation point (point._2)
+   *  has to be somewhere in-between. The weights of the two considered sample values
+   *  are adjusted accordingly (linear interpolation).
+   *  @param runId the run id
+   *  @param point the interpolation point
+   *  @param varName the variable name
+   *  @param results the results
+   */
+  private[this] def addValueForPoint(runId: Int, point: InterpolationPoint, varName: String, results: MultiTable) = {
+    require(point._1._2 <= point._2 && point._3._2 >= point._2, "Invalid interpolation point:" + point)
 
-}
+    //Get values between which shall be interpolated
+    val colIndex = results.getColumnIndex(varName)
+    val firstValue = results.getValueAt(point._1._1, colIndex)
+    val secondValue = results.getValueAt(point._3._1, colIndex)
 
-object Instrumentation {
-  val MIN_INSTRUMENTATION_PRECISION = 10e-02
+    //Calculate width of time interval
+    val timeInterval = point._3._2 - point._1._2
+
+    //Calculate weights
+    val firstWeight = (point._3._2 - point._2) / timeInterval
+    val secondWeight = 1 - firstWeight
+
+    addValueFor(runId, varName, (point._2, firstWeight * firstValue + secondWeight * secondValue))
+  }
+
 }
