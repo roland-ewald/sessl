f1699a5f43d64aa99c558897354acc3c34d068cb
__roland__ <devnull@localhost>
2012-03-10 09:17:48 +0100

    Added test on interpolation helper methods to all-tests suite.

--- src/test/scala/tests/sessl/AllTests.scala
+++ src/test/scala/tests/sessl/AllTests.scala
@@ -18,12 +18,13 @@ import tests.sessl.james.TestReport
 import tests.sessl.james.TestStoppingConditions
 import tests.sessl.james.TestReplicationConditions
 import tests.sessl.sbmlsim.TestSimpleSBMLSimExperiments
+import scala.tools.cmd.Interpolation
 
 /** Bundles all tests together (as the Eclipse-JUnit-Runner cannot easily deal with Scala code).
  *  @author Roland Ewald
  */
 @RunWith(value = classOf[org.junit.runners.Suite])
-@SuiteClasses(value = Array(classOf[TestParameterScan], classOf[TestOptimization], classOf[TestRNGSetup],
+@SuiteClasses(value = Array(classOf[Interpolation], classOf[TestParameterScan], classOf[TestOptimization], classOf[TestRNGSetup],
   classOf[TestDataSink], classOf[TestBiBJamesExperiment], classOf[TestReport], classOf[TestStoppingConditions],
   classOf[TestReplicationConditions], classOf[TestPerformanceExperiments],
   classOf[TestVariable], classOf[TestSimpleInstrumentation], classOf[ParamBlockGeneratorTest], classOf[TestSimpleSBMLSimExperiments]))
