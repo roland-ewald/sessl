0b45d96f6870db52bac48024d782610828234bc2
Roland Ewald <roland.ewald@uni-rostock.de>
2012-06-07 12:54:27 +0200

    sessl-omnetpp: started work on integration of omnetpp

---
+++ sessl-omnetpp/src/main/scala/sessl/omnetpp/Experiment.scala
@@ -0,0 +1,15 @@
+package sessl.omnetpp
+
+import sessl.AbstractExperiment
+
+/**
+ * Integrates OMNeT++ 4.2 (http://www.omnetpp.org/) by producing <code>omnetpp.ini</code> files.
+ * @author Roland Ewald
+ */
+class Experiment extends AbstractExperiment {
+
+  def basicConfiguration(): Unit = {}
+
+  def executeExperiment(): Unit = {}
+
+}
