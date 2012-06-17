e845bc5bf5d28daf31c69f38778c2ccf6c29a88b
Roland Ewald <roland.ewald@uni-rostock.de>
2012-06-17 18:06:16 +0200

    Added some notes on observation implementation.

--- sessl-omnetpp/src/main/scala/sessl/omnetpp/Observation.scala
+++ sessl-omnetpp/src/main/scala/sessl/omnetpp/Observation.scala
@@ -19,6 +19,36 @@ trait Observation extends SimpleObservation with OMNeTPPResultHandler {
     super.considerResults(runId, workingDir)
     println("I'm considering run " + runId)
 
+    //TODO Add options: 
+    //warmup-period = 20s
+    //output-vector-file = "${resultdir}/${configname}-${runnumber}.vec"
+    //output-scalar-file = "${resultdir}/${configname}-${runnumber}.sca"
+
+    //TODO: Map from observerAt(...) elements to this
+
+    //**.result-recording-modes = all
+    //recordingModes {
+    // "**" ~> All
+    // "x" ~> None
+    // "y" ~> Default
+    // "z" ~> Without("u") and With("v")
+    //}
+    //**.scalar-recording = false
+
+    // scalarRecording {
+    //   "**" ~> Disabled
+    //   "x" ~> Enabled
+    // }
+
+    //same for vectorRecording {...}
+    //+ Record intervals: **.vector-recording-intervals = 0..
+    //+ **.vector-record-eventnumbers = false
+
+    //Some notes
+    // -defenisve: by default, everything should be disabled
+    // -limited: non-numeric observations not supported by sessl so far
+    // -wildcards: a mechanism is required to 'resolve' wildcards, i.e. to register new matching variables at runtime (internal = external name) 
+    //  (for this AbstractObservation#bindings needs to be updated)
   }
 
 }
