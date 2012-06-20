c431636f81e56a65880826df4655048b2aabbc56
Roland Ewald <roland.ewald@uni-rostock.de>
2012-06-20 16:56:19 +0200

    sessl: removed redundant test case, moved string-specific test to existing test.

--- sessl-omnetpp/src/test/scala/tests/sessl/omnetpp/SimpleOMNeTPPExperiments.scala
+++ sessl-omnetpp/src/test/scala/tests/sessl/omnetpp/SimpleOMNeTPPExperiments.scala
@@ -31,7 +31,7 @@ import org.junit.Test
     }
   }
 
-  @Test def testSimpleExperiment() {
+  @Test def testTicTocExperiment() {
     import sessl._
     import sessl.omnetpp._
     execute {
@@ -41,6 +41,20 @@ import org.junit.Test
         set("Network.host[*].app.typename" <~ "TicTocApp")
         stopCondition = AfterSimTime(minutes = 10) or AfterWallClockTime(seconds = 10)
         scan("tic.out.delay" <~ range(100, 100, 1000), "tic.in.delay" <~ range(20, 20, 100) and "Network.numHosts" <~ range(20, 20, 100))
+      }
+    }
+  }
+
+  @Test def testClosedQueueingNetwork() {
+    import sessl._
+    import sessl.omnetpp._
+    execute {
+      new Experiment with EventLogRecording with Observation {
+        model = ("omnetpp-samples/cqn/cqn.exe" -> "ClosedQueueingNetA")
+        replications = 2
+        stopCondition = AfterSimTime(hours = 10) or AfterWallClockTime(seconds = 10)
+        scan("*.queue[*].numInitialJobs" <~ (2, 4, 8),
+          "*.sDelay" <~ range("%ds", 2, 2, 10) and "*.queue[*].serviceTime" <~ range("exponential(%ds)", 2, 2, 10))
       }
     }
   }
--- sessl/src/test/scala/tests/sessl/TestVariable.scala
+++ sessl/src/test/scala/tests/sessl/TestVariable.scala
@@ -14,14 +14,12 @@ import sessl._
  * @author Roland Ewald
  *
  */
-@Test
-class TestVariable {
+@Test class TestVariable {
 
   val expectedIntRangeElements = 10
   val expectedDoubleRangeElements = 19
 
-  @Test
-  def testVarRangeToList() = {
+  @Test def testVarRangeToList() = {
 
     val intRange = { "" <~ range(1, 1, 10) }.asInstanceOf[VarRange[Int]].toList
     assertEquals(expectedIntRangeElements, intRange.length)
@@ -37,6 +35,11 @@ class TestVariable {
     assertEquals(1., reverseDoubleRange(expectedDoubleRangeElements - 1))
 
     assertEquals("When lower and upper bound are the same, one element is returned.", 1, { "doubleRange" <~ range(1, 0.5, 1) }.asInstanceOf[VarRange[Double]].toList.length)
+  }
+
+  @Test def testVarRangesWithStrings() = {
+    val ranges = List(range("%d", 1, 1, 10), range("%f", 1.0, 1.0, 10.0), range("%d", 1, 10))
+    ranges.foreach(r => assertTrue(r.forall(!_.isEmpty) && r.length == 10))
   }
 
 }
