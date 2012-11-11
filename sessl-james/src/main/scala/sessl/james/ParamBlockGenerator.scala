8e9cf77856256e0323478a8c01ff9a6d307b3a06
Roland Ewald <roland.ewald@uni-rostock.de>
2012-11-11 18:47:44 +0100

    improved handling of non-standard factory parameters in james binding

--- sessl-james/src/main/scala/sessl/james/ParamBlockGenerator.scala
+++ sessl-james/src/main/scala/sessl/james/ParamBlockGenerator.scala
@@ -1,18 +1,20 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * Copyright 2012 Roland Ewald
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ * ****************************************************************************
+ */
 package sessl.james
 
 import sessl.Algorithm
@@ -20,6 +22,7 @@ import sessl.util.AlgorithmSet
 import sessl.util.CreatableFromVariables
 import java.util.logging.Level
 import james.SimSystem
+import james.core.parameters.ParameterBlock
 
 /**
  * Utility functions to generate parameter blocks.
@@ -36,8 +39,10 @@ object ParamBlockGenerator {
   def createParamBlock[T <: JamesIIAlgo[Factory]](algorithm: T): ParamBlock = {
     val paramBlock = new ParamBlock(algorithm.factory.getClass.getName)
     algorithm match {
-      case a: CreatableFromVariables[_] => a.getInternalParameters().foreach(entry => addToParamBlock(paramBlock, entry))
-      case _ => report(Level.INFO, "Algorithm " + algorithm + " cannot be parameterized.")
+      case a: CreatableFromVariables[_] =>
+        a.getInternalParameters().foreach(entry => addToParamBlock(paramBlock, entry, algorithm.customBlockName(entry._1)))
+      case _ =>
+        report(Level.INFO, "Algorithm " + algorithm + " cannot be parameterized.")
     }
     paramBlock
   }
@@ -50,11 +55,16 @@ object ParamBlockGenerator {
    *          the parameter block
    * @param nameAndValue
    *          the name and value to be specified
+   * @param customBlockName
+   * 		  if set, the custom (non-standard) name of the sub-block under which algorithm parameters are stored
    */
-  private[this] def addToParamBlock(paramBlock: ParamBlock, nameAndValue: (String, Any)): Unit = {
+  private[this] def addToParamBlock(paramBlock: ParamBlock, nameAndValue: (String, Any), customBlockName: Option[String]): Unit = {
     nameAndValue._2 match {
       case algo: JamesIIAlgo[_] =>
-        paramBlock.addSubBlock(Registry.getBaseFactoryFor(algo.factory.getClass).getName(), createParamBlock(algo))
+        paramBlock.addSubBlock(customBlockName.getOrElse(Registry.getBaseFactoryFor(algo.factory.getClass).getName()),
+          createParamBlock(algo))
+      case pb: ParameterBlock =>
+        paramBlock.addSubBl(nameAndValue._1, pb)
       case _ =>
         paramBlock.addSubBlock(nameAndValue._1, nameAndValue._2)
     }
