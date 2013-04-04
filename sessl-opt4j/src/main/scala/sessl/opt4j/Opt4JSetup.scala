a739ba83f301fcea771d097233a7cad4e013ed0f
Roland Ewald <roland.ewald@uni-rostock.de>
2013-04-04 14:31:48 +0200

    Opt4J execution works now, but the integration is still pretty ugly (via global public variables).

--- sessl-opt4j/src/main/scala/sessl/opt4j/Opt4JSetup.scala
+++ sessl-opt4j/src/main/scala/sessl/opt4j/Opt4JSetup.scala
@@ -17,10 +17,8 @@
  */
 package sessl.opt4j
 
-import java.util.Random
-import scala.Array.canBuildFrom
+import scala.collection.mutable.MapBuilder
 import org.opt4j.core.Genotype
-import org.opt4j.core.Objective.Sign
 import org.opt4j.core.Objectives
 import org.opt4j.core.genotype.CompositeGenotype
 import org.opt4j.core.genotype.DoubleGenotype
@@ -29,17 +27,24 @@ import org.opt4j.core.genotype.SelectGenotype
 import org.opt4j.core.problem.Creator
 import org.opt4j.core.problem.Decoder
 import org.opt4j.core.problem.Evaluator
-import org.opt4j.core.problem.ProblemModule
 import sessl.optimization.AbstractOptimizerSetup
+import sessl.optimization.BoundedSearchSpaceDimension
+import sessl.optimization.BoundedSearchSpaceDimension
 import sessl.optimization.BoundedSearchSpaceDimension
 import sessl.optimization.GeneralSearchSpaceDimension
 import sessl.optimization.SearchSpaceDimension
 import sessl.optimization.SimpleParameters
+import sessl.optimization.SimpleParameters
 import sessl.util.Logging
 import sessl.util.ScalaToJava
-import org.opt4j.optimizers.ea.EvolutionaryAlgorithmModule
+import java.util.Random
+import org.opt4j.core.Objective
+import org.opt4j.core.Objective.Sign
 import org.opt4j.viewer.ViewerModule
+import org.opt4j.optimizers.ea.EvolutionaryAlgorithmModule
 import org.opt4j.core.start.Opt4JTask
+import org.opt4j.core.problem.ProblemModule
+import com.google.inject.Inject
 
 /**
  * Support for Opt4J.
@@ -50,72 +55,16 @@ class Opt4JSetup extends AbstractOptimizerSetup with Logging {
 
   override def execute() = {
 
-    /**
-     *  Creates random genotype.
-     */
-    val creator = new Creator[CompositeGenotype[String, Genotype]] {
+    //From the tutorial
 
-      val rng = new Random //TODO: generalize this
-
-      /** Composite genotype: it consists of a genotype per dimension. */
-      val genotypePerDimension: Seq[(String, Genotype)] =
-        for (dim <- searchSpace) yield dim match {
-          case bounds @ BoundedSearchSpaceDimension(name, lower, stepSize, upper) =>
-            lower match {
-              case l: Double => (name, new DoubleGenotype(l, upper.asInstanceOf[Double]))
-              case l: Int => (name, new IntegerGenotype(l, upper.asInstanceOf[Int]))
-              case _ => throw new IllegalArgumentException("This type of numerical bound is not supported:" + lower.getClass)
-            }
-          case list @ GeneralSearchSpaceDimension(name, values) => (name, new SelectGenotype(ScalaToJava.toList(values)))
-          case _ => throw new IllegalArgumentException("This type of search space dimension is not supported:" + dim)
-        }
-
-      override def create(): CompositeGenotype[String, Genotype] = {
-        val composite = new CompositeGenotype(ScalaToJava.toMap(genotypePerDimension.toMap))
-        for (paramName <- composite.keySet.toArray) {
-          composite.get[Genotype](paramName) match {
-            case s: SelectGenotype[_] => s.init(rng, 1)
-            case d: DoubleGenotype => d.init(rng, 1)
-            case i: IntegerGenotype => i.init(rng, 1)
-            case x => throw new IllegalArgumentException("Genotype not supported:" + x.getClass())
-          }
-        }
-        composite
-      }
-    }
-
-    val decoder = new Decoder[CompositeGenotype[String, Genotype], SimpleParameters] {
-      override def decode(composite: CompositeGenotype[String, Genotype]): SimpleParameters = {
-        val paramAssignment =
-          for (paramName <- composite.keySet.toArray) yield (paramName.toString,
-            composite.get[Genotype](paramName) match {
-              case s: SelectGenotype[_] => s.getValue(0)
-              case d: DoubleGenotype => d.get(0)
-              case i: IntegerGenotype => i.get(0)
-              case x => throw new IllegalArgumentException("Genotype not supported:" + x.getClass())
-            })
-        SimpleParameters(paramAssignment.toMap)
-      }
-    }
-
-    val evaluator = new Evaluator[SimpleParameters] {
-      override def evaluate(params: SimpleParameters): Objectives = {
-        val objectives: Objectives = new Objectives
-        objectives.add("objective", Sign.MAX, objective(params))
-        if (params.firstUnusedParameter >= 0)
-          logger.warn("The parameter '" + params.firstUnusedParameterName.get +
-            "' has not been accessed from within the objective function. Is the configuration of the search space correct?")
-        objectives
-      }
-    }
-
+    Opt4JSetup.f = objective
+    Opt4JSetup.searchSpace = searchSpace
+    
     val problemModule = new ProblemModule() {
       override def config() {
-        bindProblem(creator.getClass, decoder.getClass, evaluator.getClass)
+        bindProblem(classOf[SimpleParameterCreator], classOf[SimpleParameterDecoder], classOf[SimpleParameterEvaluator])
       }
     }
-
-    //From the tutorial:
 
     val ea = new EvolutionaryAlgorithmModule()
     ea.setGenerations(500)
@@ -140,6 +89,75 @@ class Opt4JSetup extends AbstractOptimizerSetup with Logging {
 }
 
 object Opt4JSetup {
+  
+  var f: sessl.optimization.Objective = null
+  
+  var searchSpace:SearchSpace = null
+  
   type SearchSpace = Seq[SearchSpaceDimension[_]]
 }
 
+/**
+ *  Creates random genotype.
+ */
+class SimpleParameterCreator extends Creator[CompositeGenotype[String, Genotype]] {
+
+  val rng = new Random //TODO: generalize this
+
+  /** Composite genotype: it consists of a genotype per dimension. */
+  val genotypePerDimension: Seq[(String, Genotype)] =
+    for (dim <- Opt4JSetup.searchSpace) yield dim match {
+      case bounds @ BoundedSearchSpaceDimension(name, lower, stepSize, upper) =>
+        lower match {
+          case l: Double => (name, new DoubleGenotype(l, upper.asInstanceOf[Double]))
+          case l: Int => (name, new IntegerGenotype(l, upper.asInstanceOf[Int]))
+          case _ => throw new IllegalArgumentException("This type of numerical bound is not supported:" + lower.getClass)
+        }
+      case list @ GeneralSearchSpaceDimension(name, values) => (name, new SelectGenotype(ScalaToJava.toList(values)))
+      case _ => throw new IllegalArgumentException("This type of search space dimension is not supported:" + dim)
+    }
+
+  override def create(): CompositeGenotype[String, Genotype] = {
+    val composite = new CompositeGenotype(ScalaToJava.toMap(genotypePerDimension.toMap))
+    for (paramName <- composite.keySet.toArray) {
+      composite.get[Genotype](paramName) match {
+        case s: SelectGenotype[_] => s.init(rng, 1)
+        case d: DoubleGenotype => d.init(rng, 1)
+        case i: IntegerGenotype => i.init(rng, 1)
+        case x => throw new IllegalArgumentException("Genotype not supported:" + x.getClass())
+      }
+    }
+    composite
+  }
+}
+
+/**
+ * Decodes genotype into phenotype.
+ */
+class SimpleParameterDecoder extends Decoder[CompositeGenotype[String, Genotype], SimpleParameters] {
+  override def decode(composite: CompositeGenotype[String, Genotype]): SimpleParameters = {
+    val paramAssignment =
+      for (paramName <- composite.keySet.toArray) yield (paramName.toString,
+        composite.get[Genotype](paramName) match {
+          case s: SelectGenotype[_] => s.getValue(0)
+          case d: DoubleGenotype => d.get(0)
+          case i: IntegerGenotype => i.get(0)
+          case x => throw new IllegalArgumentException("Genotype not supported:" + x.getClass())
+        })
+    SimpleParameters(paramAssignment.toMap)
+  }
+}
+
+/**
+ * Evaluates phenotype.
+ */
+class SimpleParameterEvaluator extends Evaluator[SimpleParameters] with Logging {
+  override def evaluate(params: SimpleParameters): Objectives = {
+    val objectives: Objectives = new Objectives
+    objectives.add("objective", Sign.MAX, Opt4JSetup.f(params))
+    if (params.firstUnusedParameter >= 0)
+      logger.warn("The parameter '" + params.firstUnusedParameterName.get +
+        "' has not been accessed from within the objective function. Is the configuration of the search space correct?")
+    objectives
+  }
+}
