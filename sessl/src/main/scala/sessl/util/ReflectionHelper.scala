f57cc59158906224fad1f03b982edd6ac35afcea
*** failed to import extension mercurial_keyring: No module named mercurial_keyring <devnull@localhost>
2013-04-26 14:54:41 +0200

    ReflectionHelper is now able to deal with inner case classes (though not those nested within functions).

--- sessl/src/main/scala/sessl/util/ReflectionHelper.scala
+++ sessl/src/main/scala/sessl/util/ReflectionHelper.scala
@@ -18,29 +18,72 @@
 package sessl.util
 
 import scala.reflect._
-import scala.reflect.runtime.{currentMirror => cm}
+import scala.reflect.runtime.{ currentMirror => cm }
 import scala.reflect.runtime.universe._
 
 /**
  * Provides some helper methods for reflection.
  *
- * Some of the code has been derived from this answer at stack overflow: http://stackoverflow.com/a/13813000/109942 
- * which answers this question: http://stackoverflow.com/q/14034142/109942
- *
  * @author Roland Ewald
  */
 object ReflectionHelper {
 
-  def caseClassConstrArgs[A](a: A): Seq[(String, Any)] = {
-    caseClassConstrInfo(cm.classSymbol(a.getClass))
+  /**
+   * Get parameter names and their default values for all constructor arguments.
+   * Assumes that there are default values for each parameter.
+   *
+   * TODO: This does not work with case classes that are nested within functions
+   *
+   * @param a any product (case class instance)
+   * @return list of (parameter name, default value) tuples, in correct order
+   */
+  def caseClassConstrArgs(a: Product): Seq[(String, Any)] = {
+    
+    val classSymbol = cm.classSymbol(a.getClass)
+
+    val moduleInstanceMirror =
+      if (classSymbol.isStatic) { //Normal case class...
+        val companionObj = classSymbol.companionSymbol.asModule
+        cm.reflect(cm.reflectModule(companionObj).instance)
+      } else { //... or belongs to an inner type?
+
+        // Get instance mirror for outer type
+        val rtMirror = runtimeMirror(a.getClass.getClassLoader)
+        val outerType = getOuter(a)
+        val instMirrorOuterType = rtMirror.reflect(outerType)
+
+        // Get module mirror of inner class' companion
+        val innerClassMirror = instMirrorOuterType.reflectClass(classSymbol)
+        val companionSymbol = innerClassMirror.symbol.companionSymbol
+        val moduleMirror = instMirrorOuterType.reflectModule(companionSymbol.asModule)
+
+        // Get instance mirror for companion object
+        rtMirror.reflect(moduleMirror.instance)
+      }
+    
+    argsAndDefaults(moduleInstanceMirror, "apply")
   }
 
-  def caseClassConstrInfo(c: ClassSymbol): Seq[(String, Any)] = {
-    val companionObj = c.companionSymbol.asModule
-    argsAndDefaults(cm.reflect(cm.reflectModule(companionObj).instance), "apply")
-  }
+  /**
+   * Gets outer type of an object. Assumes the parameter is an instance of an inner class.
+   *  @param the object
+   *  @return the outer type
+   */
+  def getOuter[T](a: Any) = a.getClass.getField("$outer").get(a)
 
+  /**
+   * Given an instance mirror and a method name, return the argument names and their default values.
+   * This method requires
+   *
+   * The code has been derived from this answer at stack overflow: http://stackoverflow.com/a/13813000/109942
+   * (answered question http://stackoverflow.com/q/14034142/109942)
+   *
+   * @param im the instance mirror
+   * @param the name of the method to be instantiated
+   * @return list of (parameter name, default value) tuples, in correct order
+   */
   def argsAndDefaults(im: InstanceMirror, name: String): Seq[(String, Any)] = {
+
     val ts = im.symbol.typeSignature
     val method = ts.member(newTermName(name)).asMethod
 
--- sessl/src/test/scala/sessl/util/TestReflectionHelper.scala
+++ sessl/src/test/scala/sessl/util/TestReflectionHelper.scala
@@ -13,24 +13,41 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ****************************************************************************/
+ * ***************************************************************************
+ */
 package sessl.util
 
 import org.junit.Test
 import org.junit.Assert._
+import org.junit.Assert
+import ReflectionHelper._
+
+case class TestNormalCaseClass(val x: Int = 1, val y: String = "test")
 
 /**
  * Tests for {@link ReflectionHelper}.
- * 
+ *
  * @see ReflectionHelper
  * @author Roland Ewald
  */
 @Test class TestReflectionHelper {
 
-  case class TestExample(val x: Int = 1, val y: Double = 1.0)
+  val testString = "test"
 
-  @Test def testDynamicMethodCall() {
-    println(ReflectionHelper.caseClassConstrArgs(new TestExample()))
+  case class TestInnerCaseClass(val z: Int = 2)
+
+  @Test def testNormalCaseClassConstructorReflection() {
+    assertEquals(Seq(("x", 1), ("y", testString)), caseClassConstrArgs(TestNormalCaseClass()).toSeq)
+  }
+
+  @Test def testInnerCaseClassConstructorReflection() {
+    assertEquals(Seq(("z", 2)), caseClassConstrArgs(TestInnerCaseClass()).toSeq)
+  }
+
+  @Test(expected = classOf[ScalaReflectionException])
+  def testFunctionInnerCaseClassConstructorReflection() {
+    case class TestFunctionInnerCaseClass(val testName: Int = 4, x: String = testString)
+    assertEquals(Seq(("testName", 4), "x", testString), caseClassConstrArgs(TestFunctionInnerCaseClass()).toSeq)
   }
 
 }
