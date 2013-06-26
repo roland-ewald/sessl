1d0dc480abe5a88d91fb6ac95433b8fa85f7632f
Roland Ewald <roland.ewald@uni-rostock.de>
2013-06-26 17:16:25 +0200

    Improved scaladocs.

--- sessl/src/main/scala/sessl/AbstractDataSink.scala
+++ sessl/src/main/scala/sessl/AbstractDataSink.scala
@@ -1,21 +1,29 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * Copyright 2012 Roland Ewald
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ * ****************************************************************************
+ */
 package sessl
 
-/** Some type definitions for data sinks.
+/**
+ * Some type definitions for data sinks.
+ *
+ *  @example {{{
+ *  //...
+ *  dataSink = MyDataSink(...)
+ *  //...}}}
  *
  *  @author Roland Ewald
  */
@@ -25,8 +33,10 @@ trait AbstractDataSink extends ExperimentConfiguration {
   /** The data sink to be used. */
   protected[sessl] var dataSinkSpecification: Option[DataSinkSpecification] = None
 
-  /** Getting/setting the data sink. */
+  /** Set the data sink. */
   def dataSink_=(ds: DataSinkSpecification) = { dataSinkSpecification = Some(ds) }
+  
+  /** Get the data sink.*/
   def dataSink: DataSinkSpecification = { dataSinkSpecification.get }
 }
 
