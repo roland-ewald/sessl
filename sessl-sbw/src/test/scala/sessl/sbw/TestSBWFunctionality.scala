0befd41c725109ffc4534bb9b2b61e1dd4de68e0
Roland Ewald <roland.ewald@uni-rostock.de>
2013-08-03 10:38:42 +0200

    ALESIA-13: added assumption that SBW is installed and can be connected

--- sessl-sbw/src/test/scala/sessl/sbw/TestSBWFunctionality.scala
+++ sessl-sbw/src/test/scala/sessl/sbw/TestSBWFunctionality.scala
@@ -2,13 +2,29 @@ package sessl.sbw
 
 import org.junit.Test
 import edu.caltech.sbw.SBW
+import org.junit.Assume
+import org.junit.Before
+import org.junit.Assert._
 
 class TestSBWFunctionality {
 
+  /** Checks whether there is an SBW installation to use. */
+  val connectionSuccessful =
+    try {
+      SBW.connect
+      true
+    } catch {
+      case _: Throwable => false
+    }
+
+  @Before def setUp() = {
+    Assume.assumeTrue(connectionSuccessful);
+  }
+
   @Test def testSimpleSBWLookup() = {
-	  SBW.connect
-	  for (m <- SBW.getExistingModuleInstances())
-	    println(m.getDescriptor().getName())
+    assertTrue("There are modules.", SBW.getExistingModuleInstances().length > 0)
+    for (m <- SBW.getExistingModuleInstances())
+      println(m.getDescriptor().getName())
   }
 
 }
