cb4973cd4ba573df33eb028015cd0cd2c8a6fb2e
Roland Ewald <roland.ewald@uni-rostock.de>
2013-11-15 17:43:56 +0100

    Added convenience methods to easier access variable assignments.

--- sessl/src/main/scala/sessl/ResultAspect.scala
+++ sessl/src/main/scala/sessl/ResultAspect.scala
@@ -1,25 +1,28 @@
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * Copyright 2012 Roland Ewald
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ * ****************************************************************************
+ */
 package sessl
 
 import scala.collection.mutable.Map
 
 import sessl.util.ResultOperations
 
-/** Super type of all result aspects. A result aspect additional result data
+/**
+ * Super type of all result aspects. A result aspect additional result data
  *  that can be associated with a certain kind of result.
  *  @param R the type of result this aspect can be associate with
  */
@@ -34,7 +37,8 @@ trait ResultAspect[R <: Result] {
   /** Provides access to the general results object. Note that you may use this reference to get *another* kind of result aspect. */
   def results = result.getOrElse { throw new IllegalArgumentException("The result aspect '" + this + "' is not configured correctly (no reference to result is set).") }
 
-  /** Sets the result aspect, should be done by the result aspect management.
+  /**
+   * Sets the result aspect, should be done by the result aspect management.
    *  @see sessl.ResultAspectManagement
    */
   private[sessl] def setResult(r: R) = {
@@ -44,11 +48,15 @@ trait ResultAspect[R <: Result] {
 
 /** Super class for all run results aspects. */
 abstract class RunResultsAspect(myOwner: Result.Owner) extends ResultAspect[RunResults] {
+  
   /** The aspect id is the id of its run. */
   def id = results.id
 
   /** Return the owner. */
   override def owner = myOwner
+
+  /** Convenience method to access variable assignment. */
+  def variableAssignment = results.assignment
 }
 
 /** Super class for all replication results aspects. */
@@ -65,8 +73,11 @@ abstract class ReplicationsResultsAspect(myOwner: Result.Owner) extends ResultAs
   /** Return the owner. */
   override def owner = myOwner
 
+  /** Convenience method to access variable assignment. */
+  def variableAssignment = runsResults.head._2.variableAssignment
+
   /** Convenience variable to use when collection is processed further. */
-  lazy val runsResultsMap = runsResults.toMap
+  def runsResultsMap = runsResults.toMap
 }
 
 /** Super class for all experiment results aspects. */
@@ -82,7 +93,7 @@ abstract class ExperimentResultsAspect(myOwner: Result.Owner) extends ResultAspe
   override def owner = myOwner
 
   /** Convenience variable to use when collection is processed further. */
-  lazy val runsResultsMap = runsResults.toMap
+  def runsResultsMap = runsResults.toMap
 
   /** Will only be called once, by {@link ExperimentResults}.*/
   private[sessl] def setResults(rResults: Map[Int, RunResultsAspect], repResults: Map[Int, ReplicationsResultsAspect]) {
